e81f156c775b8483b51dd83181d08f77e44023c6
xschen <xs0040@gmail.com>
2018-01-06 15:31:39 +0800

    get non-max suppression and iou implementation

--- keras_object_detection/library/yolo.py
+++ keras_object_detection/library/yolo.py
@@ -12,10 +12,7 @@ from keras import backend as K
 from keras.layers import Input, Lambda, Conv2D
 from keras.models import load_model, Model
 from keras_object_detection.library.yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
-from yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
-
-
-# GRADED FUNCTION: yolo_filter_boxes
+from keras_object_detection.library.yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
 
 def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
     """Filters YOLO boxes by thresholding on object and class confidence.
@@ -52,3 +49,107 @@ def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
     classes = tf.boolean_mask(box_classes, filtering_mask)
 
     return scores, boxes, classes
+
+
+def yolo_filter_boxes_test():
+    with tf.Session() as test_a:
+        box_confidence = tf.random_normal([19, 19, 5, 1], mean=1, stddev=4, seed=1)
+        boxes = tf.random_normal([19, 19, 5, 4], mean=1, stddev=4, seed=1)
+        box_class_probs = tf.random_normal([19, 19, 5, 80], mean=1, stddev=4, seed=1)
+        scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=0.5)
+        print("scores[2] = " + str(scores[2].eval()))
+        print("boxes[2] = " + str(boxes[2].eval()))
+        print("classes[2] = " + str(classes[2].eval()))
+        print("scores.shape = " + str(scores.shape))
+        print("boxes.shape = " + str(boxes.shape))
+        print("classes.shape = " + str(classes.shape))
+
+
+def iou(box1, box2):
+    """Implement the intersection over union (IoU) between box1 and box2
+
+    Arguments:
+    box1 -- first box, list object with coordinates (x1, y1, x2, y2)
+    box2 -- second box, list object with coordinates (x1, y1, x2, y2)
+    """
+
+    # Calculate the (y1, x1, y2, x2) coordinates of the intersection of box1 and box2. Calculate its Area.
+    xi1 = max(box1[0], box2[0])
+    yi1 = max(box1[1], box2[1])
+    xi2 = min(box1[2], box2[2])
+    yi2 = min(box1[3], box2[3])
+    inter_area = (xi2 - xi1) * (yi2 - yi1)
+
+    # Calculate the Union area by using Formula: Union(A,B) = A + B - Inter(A,B)
+    box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    union_area = box1_area + box2_area - inter_area
+
+    # compute the IoU
+    iou = inter_area / union_area
+
+    return iou
+
+
+def iou_test():
+    box1 = (2, 1, 4, 3)
+    box2 = (1, 2, 3, 4)
+    print("iou = " + str(iou(box1, box2)))
+
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes=10, iou_threshold=0.5):
+    """
+    Applies Non-max suppression (NMS) to set of boxes
+
+    Arguments:
+    scores -- tensor of shape (None,), output of yolo_filter_boxes()
+    boxes -- tensor of shape (None, 4), output of yolo_filter_boxes() that have been scaled to the image size (see later)
+    classes -- tensor of shape (None,), output of yolo_filter_boxes()
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+
+    Returns:
+    scores -- tensor of shape (, None), predicted score for each box
+    boxes -- tensor of shape (4, None), predicted box coordinates
+    classes -- tensor of shape (, None), predicted class for each box
+
+    Note: The "None" dimension of the output tensors has obviously to be less than max_boxes. Note also that this
+    function will transpose the shapes of scores, boxes, classes. This is made for convenience.
+    """
+
+    max_boxes_tensor = K.variable(max_boxes, dtype='int32')  # tensor to be used in tf.image.non_max_suppression()
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor]))  # initialize variable max_boxes_tensor
+
+    # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_boxes_tensor, iou_threshold)
+
+    # Use K.gather() to select only nms_indices from scores, boxes and classes
+    scores = K.gather(scores, nms_indices)
+    boxes = K.gather(boxes, nms_indices)
+    classes = K.gather(classes, nms_indices)
+
+    return scores, boxes, classes
+
+
+def yolo_non_max_suppression_test():
+    with tf.Session() as test_b:
+        scores = tf.random_normal([54, ], mean=1, stddev=4, seed=1)
+        boxes = tf.random_normal([54, 4], mean=1, stddev=4, seed=1)
+        classes = tf.random_normal([54, ], mean=1, stddev=4, seed=1)
+        scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes)
+        print("scores[2] = " + str(scores[2].eval()))
+        print("boxes[2] = " + str(boxes[2].eval()))
+        print("classes[2] = " + str(classes[2].eval()))
+        print("scores.shape = " + str(scores.eval().shape))
+        print("boxes.shape = " + str(boxes.eval().shape))
+        print("classes.shape = " + str(classes.eval().shape))
+
+
+def main():
+    yolo_filter_boxes_test()
+    iou_test()
+    yolo_non_max_suppression_test()
+
+
+if __name__ == '__main__':
+    main()
