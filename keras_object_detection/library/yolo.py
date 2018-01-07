7332005a6304f44b361233cceb4d222fa7cddaf4
xschen <xs0040@gmail.com>
2018-01-07 14:21:43 +0800

    get the object detector to work

--- keras_object_detection/library/yolo.py
+++ keras_object_detection/library/yolo.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import matplotlib.pyplot as plt
+from keras.models import load_model
 from matplotlib.pyplot import imshow
 import scipy.io
 import scipy.misc
@@ -10,9 +11,11 @@ import PIL
 import tensorflow as tf
 from keras import backend as K
 from keras.layers import Input, Lambda, Conv2D
-from keras.models import load_model, Model
-from keras_object_detection.library.yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
-from keras_object_detection.library.yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
+from keras_object_detection.library.yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, \
+    draw_boxes, scale_boxes
+from keras_object_detection.library.yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, \
+    preprocess_true_boxes, yolo_loss, yolo_body
+
 
 def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=.6):
     """Filters YOLO boxes by thresholding on object and class confidence.
@@ -145,10 +148,122 @@ def yolo_non_max_suppression_test():
         print("classes.shape = " + str(classes.eval().shape))
 
 
+def yolo_eval(yolo_outputs, image_shape=(720., 1280.), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+    """
+    Converts the output of YOLO encoding (a lot of boxes) to your predicted boxes along with their scores, box coordinates and classes.
+
+    Arguments:
+    yolo_outputs -- output of the encoding model (for image_shape of (608, 608, 3)), contains 4 tensors:
+                    box_confidence: tensor of shape (None, 19, 19, 5, 1)
+                    box_xy: tensor of shape (None, 19, 19, 5, 2)
+                    box_wh: tensor of shape (None, 19, 19, 5, 2)
+                    box_class_probs: tensor of shape (None, 19, 19, 5, 80)
+    image_shape -- tensor of shape (2,) containing the input shape, in this notebook we use (608., 608.) (has to be float32 dtype)
+    max_boxes -- integer, maximum number of predicted boxes you'd like
+    score_threshold -- real value, if [ highest class probability score < threshold], then get rid of the corresponding box
+    iou_threshold -- real value, "intersection over union" threshold used for NMS filtering
+
+    Returns:
+    scores -- tensor of shape (None, ), predicted score for each box
+    boxes -- tensor of shape (None, 4), predicted box coordinates
+    classes -- tensor of shape (None,), predicted class for each box
+    """
+
+    # Retrieve outputs of the YOLO model (≈1 line)
+    box_confidence, box_xy, box_wh, box_class_probs = yolo_outputs
+
+    # Convert boxes to be ready for filtering functions
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+
+    # Use one of the functions you've implemented to perform Score-filtering with a threshold of score_threshold (≈1 line)
+    scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold=score_threshold)
+
+    # Scale boxes back to original image shape.
+    boxes = scale_boxes(boxes, image_shape)
+
+    # Use one of the functions you've implemented to perform Non-max suppression with a threshold of iou_threshold (≈1 line)
+    scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes, max_boxes=max_boxes,
+                                                      iou_threshold=iou_threshold)
+
+    return scores, boxes, classes
+
+
+def yolo_eval_test():
+    with tf.Session() as test_b:
+        yolo_outputs = (tf.random_normal([19, 19, 5, 1], mean=1, stddev=4, seed=1),
+                        tf.random_normal([19, 19, 5, 2], mean=1, stddev=4, seed=1),
+                        tf.random_normal([19, 19, 5, 2], mean=1, stddev=4, seed=1),
+                        tf.random_normal([19, 19, 5, 80], mean=1, stddev=4, seed=1))
+        scores, boxes, classes = yolo_eval(yolo_outputs)
+        print("scores[2] = " + str(scores[2].eval()))
+        print("boxes[2] = " + str(boxes[2].eval()))
+        print("classes[2] = " + str(classes[2].eval()))
+        print("scores.shape = " + str(scores.eval().shape))
+        print("boxes.shape = " + str(boxes.eval().shape))
+        print("classes.shape = " + str(classes.eval().shape))
+
+
+class YoloObjectDetector(object):
+
+    def __init__(self):
+        self.scores = None
+        self.boxes = None
+        self.classes = None
+        self.yolo_model = None
+        self.sess = K.get_session()
+        self.class_names = None
+        self.anchors = None
+        self.image_shape = (720., 1280.)
+        self.yolo_outputs = None
+
+    def load_model(self, model_dir_path):
+        self.class_names = read_classes(model_dir_path + "/coco_classes.txt")
+        self.anchors = read_anchors(model_dir_path + "/yolo_anchors.txt")
+
+        self.yolo_model = load_model(model_dir_path + "/yolo.h5")
+        print(self.yolo_model.summary())
+
+        # The output of yolo_model is a (m, 19, 19, 5, 85) tensor that needs to pass through non-trivial
+        # processing and conversion.
+        self.yolo_outputs = yolo_head(self.yolo_model.output, self.anchors, len(self.class_names))
+
+        # yolo_outputs gave you all the predicted boxes of yolo_model in the correct format.
+        # You're now ready to perform filtering and select only the best boxes.
+        self.scores, self.boxes, self.classes = yolo_eval(self.yolo_outputs, self.image_shape)
+
+    def predict(self, image_file):
+        """
+        Runs the graph stored in "sess" to predict boxes for "image_file". Prints and plots the preditions.
+
+        Arguments:
+        sess -- your tensorflow/Keras session containing the YOLO graph
+        image_file -- name of an image stored in the "images" folder.
+
+        Returns:
+        out_scores -- tensor of shape (None, ), scores of the predicted boxes
+        out_boxes -- tensor of shape (None, 4), coordinates of the predicted boxes
+        out_classes -- tensor of shape (None, ), class index of the predicted boxes
+
+        Note: "None" actually represents the number of predicted boxes, it varies between 0 and max_boxes.
+        """
+
+        # Preprocess your image
+        image, image_data = preprocess_image(image_file, model_image_size=(608, 608))
+
+        # Run the session with the correct tensors and choose the correct placeholders in the feed_dict.
+        out_scores, out_boxes, out_classes = self.sess.run([self.scores, self.boxes, self.classes],
+                                                           feed_dict={self.yolo_model.input: image_data,
+                                                                      K.learning_phase(): 0
+                                                                      })
+
+        return [image, out_scores, out_boxes, out_classes]
+
+
 def main():
     yolo_filter_boxes_test()
     iou_test()
     yolo_non_max_suppression_test()
+    yolo_eval_test()
 
 
 if __name__ == '__main__':
